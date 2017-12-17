f4e67ca4435a609240dfb148805c0ec1958b1513
Ybrin <koray@koska.at>
2017-12-17 21:27:13 +0100

    Initial Commit

---
+++ Ballet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ballet
+//
+//  Created by Koray Koska on 17.12.17.
+//  Copyright © 2017 Boilertalk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
