db8c7597b6423600a21da7c7d971b82669e1b1b8
Koray Koska <koray@koska.at>
2017-12-18 23:06:49 +0100

    Code cleanup

--- Ballet/Login/LoginViewController.swift
+++ Ballet/Login/LoginViewController.swift
@@ -12,72 +12,64 @@ import Material
 
 class LoginViewController: UIViewController {
 
+    // MARK: - Properties
+
     @IBOutlet weak var biometryButton: IconButton!
-    
+
     @IBOutlet weak var passwordField: TextField!
-    
-    @IBAction func loginButtonClicked(_ sender: Any) {
-        if passwordField.text == "123" {
-            let story = UIStoryboard(name: "Wallet", bundle: nil)
-            let viewcontroller = story.instantiateInitialViewController()
-            self.present(viewcontroller!, animated: true, completion: nil)
-        }
-    }
-    
+
+    @IBOutlet weak var loginButton: FlatButton!
+
+    // MARK: - Initialization
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        self.view.backgroundColor = ConstantHolder.primaryColorWAlpha(alpha: 0.8)
-        
-        let context = LAContext()
-        var error: NSError?
-        
-        biometryButton.isHidden = true
-        biometryButton.isEnabled = false
-        
-        loginButton.backgroundColor = ConstantHolder.secondaryColor
-        loginButton.titleColor = ConstantHolder.white
-        
-        checkBiometry(context: context, error: error)
+
+        setupUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func biometryButtonClicked(_ sender: Any) {
-        let context = LAContext()
-        var error: NSError?
-        
-        checkBiometry(context: context, error: error)
+
+    // MARK: - UI setup
+
+    private func setupUI() {
+        self.view.backgroundColor = Colors.primaryColorWAlpha(alpha: 0.8)
+
+        // Login button
+        loginButton.backgroundColor = Colors.secondaryColor
+        loginButton.titleColor = Colors.white
+
+        // Password field
+        passwordField.returnKeyType = .done
     }
-    @IBOutlet weak var loginButton: FlatButton!
-    
-    func checkBiometry(context: LAContext, error: NSError?) {
-        var error = error
+
+    // MARK: - Biometry
+
+    private func checkBiometry(context: LAContext) {
+        var error: NSError?
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
             let reason = "Identify yourself!"
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
-                [unowned self] (success, authenticationError) in
-                
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] (success, authenticationError) in
                 DispatchQueue.main.async {
                     if success {
                         // unlock wallet
                         let story = UIStoryboard(name: "Wallet", bundle: nil)
                         let viewcontroller = story.instantiateInitialViewController()
-                        self.present(viewcontroller!, animated: true, completion: nil)
+                        self?.present(viewcontroller!, animated: true, completion: nil)
                     } else {
                         let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified! Please enter use password authentication.", preferredStyle: .alert)
                         ac.addAction(UIAlertAction(title: "OK", style: .default))
-                        self.present(ac, animated: true)
-                        
-                        self.biometryButton.isEnabled = true
-                        self.biometryButton.imageView?.image = UIImage(named: "ic_fingerprint")?.withRenderingMode(.alwaysTemplate)
-                        self.biometryButton.imageView?.tintColor = Color.white
+                        self?.present(ac, animated: true)
+
+                        self?.biometryButton.isEnabled = true
+                        self?.biometryButton.imageView?.image = UIImage(named: "ic_fingerprint")?.withRenderingMode(.alwaysTemplate)
+                        self?.biometryButton.imageView?.tintColor = Color.white
                         if context.biometryType == .faceID {
-                            self.biometryButton.imageView?.image = UIImage(named: "faceid")?.withRenderingMode(.alwaysTemplate)
+                            self?.biometryButton.imageView?.image = UIImage(named: "faceid")?.withRenderingMode(.alwaysTemplate)
                         }
                     }
                 }
@@ -85,6 +77,21 @@ class LoginViewController: UIViewController {
         } else {
             // no biometry option availible (not compatible with Touch ID and/or Face ID)
             // password authentication
+        }
+    }
+
+    // MARK: - Actions
+
+    @IBAction func biometryButtonClicked(_ sender: Any) {
+        let context = LAContext()
+        checkBiometry(context: context)
+    }
+
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        if passwordField.text == "123" {
+            let story = UIStoryboard(name: "Wallet", bundle: nil)
+            let viewcontroller = story.instantiateInitialViewController()
+            self.present(viewcontroller!, animated: true, completion: nil)
         }
     }
 }
--- Ballet/Toolbox/Colors.swift
+++ Ballet/Toolbox/Colors.swift
@@ -1,23 +1,31 @@
 //
-//  ConstantHolder
+//  Colors.swift
 //  Ballet
 //
-//  Created by Ben Koksa on 12/18/17.
+//  Created by Koray Koska on 18.12.17.
 //  Copyright © 2017 Boilertalk. All rights reserved.
 //
 
 import UIKit
 
-class ConstantHolder {
+struct Colors {
+
+    /// The primary color of this application as defined by Material Design
     static let primaryColor: UIColor = UIColor(displayP3Red: 86/255, green: 86/255, blue: 86/255, alpha: 1)
+
+    /// The secondary color of this application as defined by Material Design
     static let secondaryColor: UIColor = UIColor(displayP3Red: 42/255, green: 240/255, blue: 253/255, alpha: 1)
+
+    /// White
     static let white: UIColor = UIColor.white
-    
+
+    /// Primary color with the given alpha value
     static func primaryColorWAlpha(alpha: Float) -> UIColor {
-        return UIColor(displayP3Red: 86/255, green: 86/255, blue: 86/255, alpha: CGFloat(alpha))
+        return primaryColor.withAlphaComponent(CGFloat(alpha))
     }
-    
+
+    /// Secondary color with the given alpha value
     static func secondaryColorWAlpha(alpha: Float) -> UIColor {
-        return UIColor(displayP3Red: 42/255, green: 240/255, blue: 253/255, alpha: CGFloat(alpha))
+        return secondaryColor.withAlphaComponent(CGFloat(alpha))
     }
 }
