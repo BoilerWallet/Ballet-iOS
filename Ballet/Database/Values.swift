c333f9d354e861b245de24315460ba622a7e4b51
DeveloperBen <ben@koska.at>
2017-12-25 16:35:22 +0100

    Created Account

--- Ballet/Database/Values.swift
+++ Ballet/Database/Values.swift
@@ -8,25 +8,9 @@
 
 import Foundation
 
-class walletAccount {
-    var public_key: String
-    var name: String
-    var wei: Decimal
-    
-    init (public_key: String, name: String, wei: Decimal) {
-        self.public_key = public_key
-        self.name = name
-        self.wei = wei
-    }
-    
-    func asTxtMsg() -> String{
-        return "\(self.name) - \(self.wei / Values.weiPerEther) ETH"
-    }
-    //TODO: Implement function to get Blockie UIImage
-}
-
 class Values {
-    static let defaultAccount = walletAccount(public_key: "0xa8fa5dd30a87bb9e3288d604eb74949c515ab66e", name: "Default", wei: weiPerEther)
-    static let accounts: [walletAccount] = [defaultAccount]
+    static let defaultAccount = Account(public_key: "0xa8fa5dd30a87bb9e3288d604eb74949c515ab66e", name: "Default", wei: weiPerEther)
+    // static let emptyAccount =
+    static let accounts: [Account] = [defaultAccount]
     static let weiPerEther = pow(10, 18)
 }
--- Ballet/Send/SendViewController.swift
+++ Ballet/Send/SendViewController.swift
@@ -44,7 +44,7 @@ class SendViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func loadAccount(_ account: walletAccount) {
+    func loadAccount(_ account: Account) {
 //        let blockie = Blockies(seed: account.public_key, size: 8, scale: 3)
 //        let img = blockie.createImage(customScale: 2)
 //        blockieImageView.image = img
