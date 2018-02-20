37794157efcec1b1e2415062fee2ec5f34d38913
Koray Koska <koray@koska.at>
2018-02-20 21:19:28 +0100

    Initial loading animation view

---
+++ Ballet/Toolbox/LoadingView/LoadingView.swift
@@ -0,0 +1,25 @@
+//
+//  LoadingView.swift
+//  Ballet
+//
+//  Created by Koray Koska on 20.02.18.
+//  Copyright © 2018 Boilertalk. All rights reserved.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    @IBOutlet var contentView: UIView!
+
+    @IBOutlet weak var backgroundView: UIView!
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
