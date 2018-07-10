f3a637019ab60b12c765f896e89743e88943133c
Ybrin <koray@koska.at>
2018-07-10 15:42:42 +0200

    Add send select currency

---
+++ Ballet/Send/SendSelectCurrencyViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SendSelectCurrencyViewController.swift
+//  Ballet
+//
+//  Created by Koray Koska on 09.07.18.
+//  Copyright © 2018 Boilertalk. All rights reserved.
+//
+
+import UIKit
+
+class SendSelectCurrencyViewController: UIViewController {
+
+    // MARK: - Properties
+
+    @IBOutlet weak var currencyTableView: SettingsTokenTrackerTableView!
+
+    // MARK: - Initialization
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - UI setup
+
+    private func setupUI() {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
